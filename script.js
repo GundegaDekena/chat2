07bd9495ee149316a30a339f5a3afad962b3b98e
Gundega <gundega.dekena@gmail.com>
2022-03-18 16:13:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const API = "https://chat2022.gundegadekena.repl.co"
 
 let zina = document.querySelector('.manaZina');
 let zinas = document.querySelector('.chataZinas');
+let vards = document.querySelector('.vards');
+
 
 function sutitZinu()
 {
     console.log('sutitZinu() darbojas');
 
     zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
+
+    fetch(API+'/sutit/'+vards.value+'/'+zina.value)
 }
 
 async function ieladetChataZinas()
@@ -17,5 +21,26 @@ async function ieladetChataZinas()
     zinas.innerHTML = dati;
 }
 
-setInterval( ieladetChataZinas, 1000 )
+//setInterval( ieladetChataZinas, 1000 )
 
+async function ieladetChataZinasJson()
+{
+    let datiNoServera = await fetch(API + '/lasit');
+    let dati = await datiNoServera.json();
+    
+    //console.log(await dati[0]['zina'] )
+    zinas.innerHTML = '';
+    
+    i = 0;
+    while ( i < await dati.length )
+    {
+        //console.log(i);
+        zinas.innerHTML = zinas.innerHTML+dati[i]['vards']+': '+dati[i]['zina']+'<br />';
+
+        i = i+1;
+    }
+
+    zinas.scrollTop = zinas.scrollHeight;
+}//beidzas ieladetChataZinasJson()
+
+setInterval( ieladetChataZinasJson, 1000 )
